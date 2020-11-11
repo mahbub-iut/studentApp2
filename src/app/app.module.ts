4cf7a57d7c6bbff456cdabaab712747fe290050d
Mahbubul Alam Palash <mahbubulalam@iut-dhaka.edu>
2020-11-11 18:09:00 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/second/second.component.ts
@@ -0,0 +1,81 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-second',
+  templateUrl: './second.component.html',
+  styleUrls: ['./second.component.css']
+})
+export class SecondComponent {
+
+  // title = 'Angular Form Validation Tutorial';
+  readonly ROOT_URL = "https://jsonplaceholder.typicode.com";
+  posts: Observable<any[]> | undefined;
+  angForm: FormGroup;
+  // websiteList: any = [
+  //   { id: 1, name: 'Students' },
+  //   { id: 2, name: 'Location' },
+  //   { id: 3, name: 'Campus' },
+  //   { id: 4, name: 'Dorm' },
+  //   { id: 5, name: 'Atmsosphere' },
+  //   { id: 6, name: 'Campus' },
+  // ];
+
+  // likes: any = ['Very likely', 'Likely', 'Unlikely'];
+
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    // this.createForm();
+  }
+  // createForm() {
+  //   this.angForm = this.fb.group({
+  //     first_name: ['', Validators.required],
+  //     last_name: ['', Validators.required],
+  //     address: ['', Validators.required],
+  //     city: ['', Validators.required],
+  //     state: ['', Validators.required],
+  //     zip: ['', Validators.required],
+  //     tel_num: ['', Validators.required],
+  //     email: ['', Validators.required],
+  //     dos: ['', Validators.required],
+  //     likings: this.fb.array([], [Validators.required]),
+  //     likelihood: new FormControl('', Validators.required),
+  //     interested: new FormControl('', Validators.required)
+  //   });
+  // }
+  // get f() {
+  //   return this.angForm.controls;
+  // }
+  // onCheckboxChange(e) {
+  //   const likes: FormArray = this.angForm.get('likings') as FormArray;
+
+  //   if (e.target.checked) {
+  //     likes.push(new FormControl(e.target.value));
+  //   } else {
+  //     const index = likes.controls.findIndex(x => x.value === e.target.value);
+  //     likes.removeAt(index);
+  //   }
+  // }
+
+
+  getPosts() {
+    this.posts = this.http.get<any[]>(this.ROOT_URL + '/posts');
+    // this.http.get(this.ROOT_URL + '/posts')
+    //   .subscribe((posts: any[]) => {
+    //     this.angForm = this.fb.group({
+    //       capabilities: this.fb.array(posts.map(datum => this.generateDatumFormGroup(datum)))
+    //     });
+    //   });
+  }
+  
+
+
+
+
+
+
+
+
+}
